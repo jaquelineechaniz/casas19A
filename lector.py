ae6ee37b07616e655fa6c7962fa397ad3694bebc
fernandasalas <ArroyoSeco20>
2019-03-06 14:34:05 -0600

    se guardan las imagenes en la base de datos

--- lector.py
+++ lector.py
@@ -59,12 +59,22 @@ def insertar_fecha(file):
     return cursor.lastrowid
     conexion.commit()
 
+def insertar_imagen(url, id_casa):
+    insert = 'INSERT INTO imagen(ubicacion, id_bienraiz)'\
+             'VALUES (%s, %s)'
+    cursor.execute(insert, (url, id_casa))
+    conexion.commit()
+
 def insertar_bienraiz(casa, id_colonia, id_fecha):
     insert = 'INSERT INTO bienraiz(titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha ) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     cursor.execute(insert, (
     casa['titulo'], casa['precio'], casa['m2'], casa['recamaras'], casa['wc'], casa['cars'], casa['descripcion'],
     tipo[casa['tipo']], 1, id_colonia, id_fecha))
     conexion.commit()
+
+    id_casa = cursor.lastrowid
+    for img in casa['imgs']:
+        insertar_imagen(img, id_casa)
 
 def get_id_colonia(colonia, municipio):
     select = 'SELECT * from colonia  WHERE nombre = %s'
